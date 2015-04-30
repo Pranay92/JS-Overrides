73bd80740cd3bc04a71643e2c065beeb05fb8c18
Pranay92 <pranaydubey3009@gmail.com>
2015-04-30 11:11:20 +0530

    Updates on object overrides

--- src/Object.js
+++ src/Object.js
@@ -22,3 +22,12 @@ Object.prototype.hasOwnProperty = function(key) {
 var obj = {'name' : 'something'};
 
 console.log(obj.hasOwnProperty('name'));
+
+
+Object.prototype.toString = function() {
+	var str = new String(this);
+	return this.valueOf();
+};
+
+console.log(obj.toString());
+
